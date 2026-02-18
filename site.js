bddf3590aace06f2c01d67cd14176faf82abdfc6
innosol <innovativesolgh@gmail.com>
2026-02-18 16:35:25 +0000

    update made

--- site.js
+++ site.js
@@ -44,20 +44,45 @@ const rooms = document.querySelectorAll(".room-details-btn");
 const roomType = document.getElementById("roomModalLabel");
 const roomDescription = document.querySelector(".room-carousel-text");
 
+const firstCarouselImg = document.querySelector(".carousel-img1");
+const secondCarouselImg = document.querySelector(".carousel-img2");
+const thirdCarouselImg = document.querySelector(".carousel-img3");
+const fourthCarouselImg = document.querySelector(".carousel-img4");
+
 rooms.forEach(function (room) {
   room.addEventListener("click", function () {
     if (room.value === "apartment 1") {
       roomType.innerHTML = "Apartment 1";
       roomDescription.innerHTML = "Fully furnished room";
+
+      firstCarouselImg.src = "Images/rooms/apartment1/img_1.JPG";
+      secondCarouselImg.src = "Images/rooms/apartment1/img_2.JPG";
+      thirdCarouselImg.src = "Images/rooms/apartment1/img_3.JPG";
+      fourthCarouselImg.src = "Images/rooms/apartment1/img_4.JPG";
     } else if (room.value === "apartment 2") {
       roomType.innerHTML = "Apartment 2";
       roomDescription.innerHTML = "Fully furnished room";
+
+      firstCarouselImg.src = "Images/rooms/apartment2/img_1.JPG";
+      secondCarouselImg.src = "Images/rooms/apartment2/img_2.JPG";
+      thirdCarouselImg.src = "Images/rooms/apartment2/img_3.JPG";
+      fourthCarouselImg.src = "Images/rooms/apartment2/img_4.JPG";
     } else if (room.value === "apartment 3") {
       roomType.innerHTML = "Apartment 3";
       roomDescription.innerHTML = "Fully furnished room";
+
+      firstCarouselImg.src = "Images/rooms/apartment3/img_1.JPG";
+      secondCarouselImg.src = "Images/rooms/apartment3/img_2.JPG";
+      thirdCarouselImg.src = "Images/rooms/apartment3/img_3.JPG";
+      fourthCarouselImg.src = "Images/rooms/apartment3/img_4.JPG";
     } else if (room.value === "apartment 4") {
       roomType.innerHTML = "Apartment 4";
       roomDescription.innerHTML = "Fully furnished room";
+
+      firstCarouselImg.src = "Images/rooms/apartment4/img_1.JPG";
+      secondCarouselImg.src = "Images/rooms/apartment1/img_2.JPG";
+      thirdCarouselImg.src = "Images/rooms/apartment1/img_3.JPG";
+      fourthCarouselImg.src = "Images/rooms/apartment1/img_4.JPG";
     }
   });
 });
